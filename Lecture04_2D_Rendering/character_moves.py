90d2b5adcb29d0cec553ac7f3e9a367b17d6aacd
GODhanQ <hg.min2018@gmail.com>
2025-09-18 20:39:14 +0900

    캐릭터 출력 완료

--- Lecture04_2D_Rendering/character_moves.py
+++ Lecture04_2D_Rendering/character_moves.py
@@ -1,7 +1,10 @@
 from pico2d import *
-import math
 
 open_canvas()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+boy = load_image('character.png')
 
 def Move_rectangle():
     print("Moving rectangle")
@@ -9,11 +12,17 @@ def Move_rectangle():
 
 def Move_circle():
     print("Moving circle")
+
+    clear_canvas()
+    boy.draw_now(400,300)
+    delay(0.1)
+
     pass
 
 while True:
     Move_rectangle()
     Move_circle()
+    break
     pass
 
 close_canvas()
