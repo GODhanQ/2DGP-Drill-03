68b0894b8d6a6b180364113b08fc351f1498a509
GODhanQ <hg.min2018@gmail.com>
2025-09-18 20:52:43 +0900

    top line 완성

--- Lecture04_2D_Rendering/character_moves.py
+++ Lecture04_2D_Rendering/character_moves.py
@@ -7,15 +7,21 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 boy = load_image('character.png')
 
 def move_top():
+    print('Moving top')
+    for x in range(0, 800, 10):
+        draw_boy(x, 550)
     pass
 
 def move_right():
+    print('Moving right')
     pass
 
 def move_bottom():
+    print('Moving bottom')
     pass
 
 def move_left():
+    print('Moving left')
     pass
 
 def Move_rectangle():
@@ -35,10 +41,15 @@ def Move_circle():
     for deg in range(0, 360):
         x = r * math.cos(math.radians(deg)) + 400
         y = r * math.sin(math.radians(deg)) + 300
-        clear_canvas()
-        boy.draw_now(x,y)
-        delay(0.1)
+        draw_boy(x, y)
     pass
+
+
+def draw_boy(x: float, y: float):
+    clear_canvas()
+    boy.draw_now(x, y)
+    delay(0.1)
+
 
 while True:
     Move_rectangle()
